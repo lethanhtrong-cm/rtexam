b13e98c72a25d992fdb29cb569b654facc8df7cd
ktv3mien <kythuatvien3mien@gmail.com>
2026-07-08 11:39:00 +0700

    Update firebase-config.js

--- admin/firebase-config.js
+++ admin/firebase-config.js
@@ -1,1 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyDqdo_DJIWa5iqxiCgBq-0iGX7f9sr6soo",
+    authDomain: "rt-examination.firebaseapp.com",
+    databaseURL: "https://rt-examination-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "rt-examination",
+    storageBucket: "rt-examination.firebasestorage.app",
+    messagingSenderId: "920482699854",
+    appId: "1:920482699854:web:44f9b0d735bdc001c6c11f",
+    measurementId: "G-8N7RTTREQM"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
