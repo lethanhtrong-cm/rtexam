82e25ba00b3ddaf60953f68845887ab76a2d5e88
ktv3mien <kythuatvien3mien@gmail.com>
2026-07-19 17:47:08 +0700

    Create firebase-core.js

---
+++ js/dashboard/firebase-core.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+
+// =========================================================================
+// 1. CẤU HÌNH & KHỞI TẠO FIREBASE CHUYÊN BIỆT
+// =========================================================================
+const firebaseConfig = {
+    apiKey: "AIzaSyDqdo_DJIWa5iqxiCgBq-0iGX7f9sr6soo",
+    authDomain: "rt-examination.firebaseapp.com",
+    databaseURL: "https://rt-examination-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "rt-examination",
+    storageBucket: "rt-examination.firebasestorage.app",
+    messagingSenderId: "920482699854",
+    appId: "1:920482699854:web:44f9b0d735bdc001c6c11f",
+    measurementId: "G-8N7RTTREQM"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
