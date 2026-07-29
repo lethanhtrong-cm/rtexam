d3af44615ceda43672c24afad5c108c4a719d212
ktv3mien <kythuatvien3mien@gmail.com>
2026-07-29 09:28:28 +0700

    Create exam-state.js

---
+++ js/dashboard/exam-state.js
@@ -0,0 +1,12 @@
+// Quản lý trạng thái toàn cục cho toàn bộ Module Đề Thi
+export const State = {
+    allExamsData: [],
+    currentUserData: null,
+    currentView: 'grid',
+    currentTechnique: 'all',
+    currentLevel: 'all',
+    currentTime: 'all',
+    currentSearchQuery: '',
+    completedExams: {},
+    currentShareExamId: null
+};
