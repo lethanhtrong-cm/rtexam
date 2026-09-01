b2ad7e5df1ba90fc1df3e4ffb6f3b7128077557e
ktv3mien <kythuatvien3mien@gmail.com>
2026-09-01 15:05:01 +0700

    Create firebase-config.js

---
+++ pptx/firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDqdo_DJIWa5iqxiCgBq-0iGX7f9sr6soo",
+    authDomain: "rt-examination.firebaseapp.com",
+    projectId: "rt-examination",
+    storageBucket: "rt-examination.firebasestorage.app",
+    messagingSenderId: "920482699854",
+    appId: "1:920482699854:web:44f9b0d735bdc001c6c11f",
+    measurementId: "G-8N7RTTREQM"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
