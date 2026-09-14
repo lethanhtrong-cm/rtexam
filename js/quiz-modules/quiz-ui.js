ea4999bfbdc46352612df4275249aa2bffc0de8d
ktv3mien <kythuatvien3mien@gmail.com>
2026-09-14 21:50:45 +0700

    Update quiz-ui.js

--- js/quiz-modules/quiz-ui.js
+++ js/quiz-modules/quiz-ui.js
@@ -554,7 +554,7 @@ export function initQuizUI(db, ctx, actions) {
                 globalAiTier = userData.vipTier || 'free';
                 isUserPro = (globalAiTier === 'pro');
                 
-                if (globalAiTier === 'plus') maxLimit = 1;
+                if (globalAiTier === 'plus') maxLimit = 3;
                 else if (globalAiTier === 'pro') maxLimit = Infinity;
                 else maxLimit = 0;
 
